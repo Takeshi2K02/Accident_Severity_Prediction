5f059f3cd3f215e092935800d43913170b2f89e4
Takeshi Dilshan <115790936+Takeshi2K02@users.noreply.github.com>
2024-10-01 15:20:10 +0530

    Commit Changes

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Campus\Takeshi\Campus\SLIIT\3rd Year\1st Semester\Modules\FDM\Mini Project\Website\env
+set VIRTUAL_ENV=D:\Campus\Takeshi\Campus\SLIIT\3rd Year\1st Semester\Modules\FDM\Mini Project\Website\Accident_Severity_Prediction\env
 
 if not defined PROMPT set PROMPT=$P$G
 
